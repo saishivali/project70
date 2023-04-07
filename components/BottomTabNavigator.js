30b588ccc0254585947e98744147880e06a75129
saishivali <84268745+saishivali@users.noreply.github.com>
2023-04-07 18:21:46 +0530

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -30,11 +30,11 @@ export default class BottomTabNavigator extends Component {
               //   iconName = "time";
               // }
 
-              // if (route.name === Ride) {
-              //   iconName = "bicycle";
-              // } else if (route.name === Ride History) {
-              //   iconName = "time";
-              // }
+               if (route.name === Ride) {
+                 iconName = "bicycle";
+              } else if (route.name === Ride History) {
+                 iconName = "time";
+               }
 
                if (route.name === "Ride") {
                  iconName = "bicycle";
